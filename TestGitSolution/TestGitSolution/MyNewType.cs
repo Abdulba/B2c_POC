41a797b6fe45ab394767d7b8b70a9985ffb7a698
Sarathi R <rsarathi.nkl@gmail.com>
2019-11-28 11:59:46 +0530

    This is to test the new File add POC

---
+++ TestGitSolution/TestGitSolution/MyNewType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace TestGitSolution
+{
+    public class MyNewType
+    {
+        public void myDisplay()
+        {
+            WriteLine("This is to test the static using statement");
+        }
+    }
+}
