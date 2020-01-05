bc69835845fe7d38d42ca3cb72d00be960332bf2
Sarathi R <rsarathi.nkl@gmail.com>
2020-01-05 19:42:05 +0530

    This is to Share Singleton Design pattern

---
+++ DesignPatters/DesignPattern/Logging.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DesignPattern
+{
+    //Singleton pattern implementation
+    public class Logging
+    {
+        private Logging() { }
+
+        private static Logging LoggingInstance;
+
+        public static Logging Instance
+        {
+            get
+            {
+                if (LoggingInstance == null)
+                {
+                    //Only if the object is null
+                    LoggingInstance = new Logging();
+                    WriteLine("Instance Is Created");
+                }
+                return LoggingInstance;
+            }
+        }
+        public void myLogging(string msg)
+        {
+            Console.WriteLine("My logging: {0}", msg);
+        }
+
+        public void myPrint()
+        {
+            WriteLine("Logging Print");
+        }
+
+    }
+}
---
+++ DesignPatters/DesignPattern/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    class DesignImplementation
+    {
+        static Logging DesignObjectlog;
+        static DesignImplementation()
+        {
+            DesignObjectlog = Logging.Instance;
+        }
+        public void designPrint()
+        {
+            DesignObjectlog.myLogging("Design Print");
+        }
+    }
+    class Program
+    {
+        //class I will be having two constructor by default
+        //static memeber of class
+        static Logging myStaticLog;
+        //Instance memeber of class
+        Logging myInstanceLog;
+        public Program()
+        {
+            myInstanceLog = Logging.Instance;
+        }
+        static Program()
+        {
+            myStaticLog = Logging.Instance;
+        }        
+        static void Main(string[] args)
+        {
+            //pagelog = Logging.Instance;
+            //Logging pagelog = Logging.Instance;
+            //pagelog.myLogging();
+            //pagelog.myPrint();
+            Program.myStaticLog.myLogging("Program Message");
+        }
+    }
+}
